7a35aa5f806979a50a65c2a9eb7199ff24ba379b
Jean Pierre <jeanp413@hotmail.com>
2025-02-25 21:57:51 +0000

    Add vscode-browser feature

---
+++ src/vscode-browser/README.md
@@ -0,0 +1,14 @@
+
+# VS Code Browser
+
+Install VS Code Browser in an environment.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/jeanp413/devcontainer-features/vscode-browser:1": {
+        "version": "latest"
+    }
+}
+```
---
+++ src/vscode-browser/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+error() {
+  echo "$1" >&2
+  echo "Exiting..." >&2
+  exit 1
+}
+
+arch_detect() {
+  if [ "$(uname -m)" = "x86_64" ]; then
+    ARCH="amd64"
+  elif [ "$(uname -m)" = "aarch64" || "$(uname -m)" = "arm64" ]; then
+    ARCH="arm64"
+  else
+    error "Unsupported architecture: $(uname -m)"
+  fi
+}
+
+arch_detect
+
+GITPOD_DIR="/usr/local/gitpod/shared"
+INSTALL_PATH="/usr/local/gitpod/shared/vscode-browser/vscode-browser-agent"
+BINARY_URL="https://gitpod-flex-releases.s3.amazonaws.com/vscode-browser/latest/vscode-browser-agent-$ARCH"
+install() {
+    mkdir -p "$(dirname $INSTALL_PATH)"
+    curl -fsSL "$BINARY_URL" -o "$INSTALL_PATH"
+    chmod +x "$INSTALL_PATH"
+}
+
+if [ ! -d "$GITPOD_DIR" ]; then
+    echo "Gitpod Environment not detected, skipping installation..."
+    exit 0
+fi
+
+echo "Istalling VS Code Browser feature..."
+
+install
